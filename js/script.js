4102636abba28a6a28e22e79e04bd36f196b2f17
Mihail <m.ulanovych@gmail.com>
2022-04-04 13:36:29 +0300

    Add script menu and animate him

---
+++ js/script.js
@@ -0,0 +1,39 @@
+window.addEventListener('DOMContentLoaded', () => {
+	const tabs = document.querySelectorAll('.tabheader__item');
+	const tabsContent = document.querySelectorAll('.tabcontent');
+	const tabsParent = document.querySelector('.tabheader__items');
+
+	function hideTabContent() {
+		tabsContent.forEach(item => {
+			item.classList.add('hide');
+			item.classList.remove('show', 'fade');
+		});
+
+		tabs.forEach(item => {
+			item.classList.remove('tabheader__item_active'); // точку мы не ставим потому и так работаем с класами
+		});
+	}
+
+	function showTabContent(i = 0) {
+		tabsContent[i].classList.add('show', 'fade');
+		tabsContent[i].classList.remove('hide');
+		tabs[i].classList.add('tabheader__item_active');
+	}
+	//в ес6 полявилась возможность ставить по умолчанию к примеру мы указываем оргумент i =0 он будет по умолчанию выбирать первое меню при условии если в вызваной функции нет значения
+
+	hideTabContent();
+	showTabContent();
+
+	tabsParent.addEventListener('click', event => {
+		const target = event.target;
+
+		if (target && target.classList.contains('tabheader__item')) {
+			tabs.forEach((item, i) => {
+				if (target == item) {
+					hideTabContent();
+					showTabContent(i);
+				}
+			});
+		}
+	});
+});
