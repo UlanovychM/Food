926c2462704f5ce58ebffa3052c18047523d120c
Mihail <m.ulanovych@gmail.com>
2022-04-15 12:10:56 +0300

    Create the slider and update dp.json

--- js/script.js
+++ js/script.js
@@ -195,41 +195,65 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	}
 
+	const getResource = async url => {
+		const res = await fetch(url);
+
+		if (!res.ok) {
+			throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+		}
+
+		return await res.json();
+	};
+
+	// getResource('http://localhost:3000/menu').then(data => {
+	// data.forEach(({ img, altimg, title, descr, price }) => {
+	// 	new MenuCard(
+	// 		img,
+	// 		altimg,
+	// 		title,
+	// 		descr,
+	// 		price,
+	// 		'.menu .container'
+	// 	).render();
+	// });
+	// });
+
+	// getResource('http://localhost:3000/menu').then(data => createCard(data));
+	// function createCard(data) {
+	// 	data.forEach(({ img, altimg, title, descr, price }) => {
+	// 		const element = document.createElement('div');
+	// 		element.classList.add('menu__item');
+	// 		element.innerHTML = `
+	// 					<img src=${img} alt=${altimg} />
+	// 					<h3 class="menu__item-subtitle">${title}</h3>
+	// 					<div class="menu__item-descr">
+	// 						${descr}
+	// 					</div>
+	// 					<div class="menu__item-divider"></div>
+	// 					<div class="menu__item-price">
+	// 						<div class="menu__item-cost">Цена:</div>
+	// 						<div class="menu__item-total"><span>${price}</span> грн/день</div>
+	// 					</div>`;
+
+	// 		document.querySelector('.menu .container').append(element);
+	// 	});
+	// }  // 2 способ перебор элементов и создание карточек на сайте
+
 	// const div = new MenuCard();
 	// div.render();
 
-	new MenuCard(
-		'img/tabs/vegy.jpg',
-		'vegy',
-		'Меню "Фитнес"',
-		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-		9,
-		'.menu .container',
-		'menu__item',
-		'big'
-	).render();
-
-	new MenuCard(
-		'img/tabs/elite.jpg',
-		'elite',
-		'Меню “Премиум”',
-		'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-		14,
-		'.menu .container',
-		'menu__item',
-		'big'
-	).render();
-
-	new MenuCard(
-		'img/tabs/post.jpg',
-		'post',
-		'Меню "Постное"',
-		'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-		21,
-		'.menu .container',
-		'menu__item',
-		'big'
-	).render();
+	axios.get('http://localhost:3000/menu').then(data => {
+		data.data.forEach(({ img, altimg, title, descr, price }) => {
+			new MenuCard(
+				img,
+				altimg,
+				title,
+				descr,
+				price,
+				'.menu .container'
+			).render();
+		});
+	});
 
 	//Forms
 	const forms = document.querySelectorAll('form');
@@ -241,10 +265,20 @@ window.addEventListener('DOMContentLoaded', () => {
 	};
 
 	forms.forEach(item => {
-		postData(item);
+		bindPostData(item);
 	});
 
-	function postData(form) {
+	const postData = async (url, data) => {
+		const res = await fetch(url, {
+			method: 'POST',
+			headers: { 'Content-type': 'application/JSON' },
+			body: data,
+		});
+
+		return await res.json();
+	};
+
+	function bindPostData(form) {
 		form.addEventListener('submit', e => {
 			e.preventDefault();
 
@@ -254,20 +288,13 @@ window.addEventListener('DOMContentLoaded', () => {
 			display: block; 
 			margin: 0 auto;`;
 
-			form.insertAdjaentElement('afterend', statusMassage);
+			form.insertAdjacentElement('afterend', statusMassage);
 
 			const formData = new FormData(form);
 
-			const object = {};
-			formData.forEach(function (value, key) {
-				object[key] = value;
-			});
+			const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-			fetch('server.php', {
-				method: 'POST',
-				headers: { 'Content-type': 'application/JSON' },
-				body: JSON.stringify(object),
-			})
+			postData('http://localhost:3000/requests', json)
 				.then(data => data.text())
 				.then(data => {
 					console.log(data);
@@ -287,7 +314,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	function showThanksModal(messege) {
 		const prevModalDialog = document.querySelector('.modal__dialog ');
 
-		prevModalDialog.classList.add('hide ');
+		prevModalDialog.classList.add('hide');
 		openModal();
 
 		const thanksModal = document.createElement('div');
@@ -306,4 +333,57 @@ window.addEventListener('DOMContentLoaded', () => {
 			closeModal();
 		}, 4000);
 	}
+
+	fetch('http://localhost:3000/menu')
+		.then(data => data.json())
+		.then(res => console.log(res));
+
+	//Slider
+
+	let slideIndex = 1;
+	const slides = document.querySelectorAll('.offer__slide');
+	const prev = document.querySelector('.offer__slider-prev');
+	const next = document.querySelector('.offer__slider-next');
+	const total = document.querySelector('#total');
+	const current = document.querySelector('#current');
+
+	showSlides(slideIndex);
+
+	if (slides.length < 10) {
+		total.textContent = `0${slides.length}`;
+	} else {
+		total.textContent = slides.length;
+	}
+
+	function showSlides(n) {
+		if (n > slides.length) {
+			slideIndex = 1;
+		}
+
+		if (n < 1) {
+			slideIndex = slides.length;
+		}
+
+		slides.forEach(item => (item.style.display = 'none'));
+
+		slides[slideIndex - 1].style.display = 'block';
+
+		if (slides.length < 10) {
+			current.textContent = `0${slideIndex}`;
+		} else {
+			current.textContent = slideIndex;
+		}
+	}
+
+	function plusSlides(n) {
+		showSlides((slideIndex += n));
+	}
+
+	prev.addEventListener('click', () => {
+		plusSlides(-1);
+	});
+
+	next.addEventListener('click', () => {
+		plusSlides(1);
+	});
 });
